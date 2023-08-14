5ede606de41f8660e6508f6389b40a24fe49bc34
Orestes Febles <ofebles@gmail.com>
2023-08-14 01:35:04 +0100

    added new sections

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			sans: ['Orpheus Pro', 'sans-serif'],
+			sans: ['Inter', 'sans-serif'],
 		},
 		extend: {},
 
